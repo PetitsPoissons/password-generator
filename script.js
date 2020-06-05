a89e7614b0666bf0ebc63cfe8b3c724d06eb8656
Sandrine Poissonnet <petitspoissons@icloud.com>
2020-06-04 17:55:02 -0700

    restore original file

--- script.js
+++ script.js
@@ -1,33 +1,3 @@
-var passwordObj = {
-  length: getPasswordLength(),
-  //lowerCase: getLowerCase(),
-  //upperCase: getUpperCase(),
-  //numChar: getNumeric(),
-  //specialChar: getSpecialChar()
-}
-/*
-// utility function to check for null user answers
-function nullAnswer(userInput) {
-  if (userInput === "" || userInput === null) {
-    return true;
-  } else {
-    return false;
-  }
-}*/
-
-// function to set and validate password length
-function getPasswordLength () {
-  var userAnswer = "";
-  while (!parseInt(userAnswer)) {
-    userAnswer = window.prompt("Choose your password length (between 8 and 128 characters");
-  };
-  if 
-}
-
-function generatePassword() {
-
-}
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -41,7 +11,5 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-//generateBtn.addEventListener("click", writePassword);
-
-console.log(passwordObj.length);
+generateBtn.addEventListener("click", writePassword);
 
